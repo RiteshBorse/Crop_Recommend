fc6892754acbe06829427dcfc04a872dd0c083da
vedika yadav <vdyadav370822@kkwagh.edu.in>
2025-01-17 21:57:32 +0530

    added config

---
+++ config.js
@@ -0,0 +1,4 @@
+
+export const CITY = "Sangli";
+export const API_KEY = `580cb0981ce9a294a111aa2343a073b1`;
+export const API_URL =  `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric` ;
